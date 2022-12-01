377dcdd79b6c22f8bd017551efde53ec5a00c339
Haf <haf@protonmail.ch>
2022-12-01 16:48:22 +0100

    added flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Types for the immutable-tuple package";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in
+  {
+    devShell.${system} = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        nodejs
+        nodePackages.npm
+      ];
+    };
+  };
+}
